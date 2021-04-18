58cbb117f4cbd94c160001b70d0a410e3f28c174
DeletedC <CurtisWoodsArt@gmail.com>
2021-04-18 15:15:45 -0400

    Added notice for noDatabase, updated comments fixed conditional render logic

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,35 +5,56 @@ class Index extends React.Component {
     render() {
 
         const {todoList} = this.props;
+        console.log(todoList);
 
         const emptyNotice = (
             <h3>There are no todos</h3>
+        );
+
+        const noDatabaseNotice = (
+            <div>
+                <h3>The database is not connected.</h3>
+                <p>Please try refreshing the page.</p>
+            </div>
         );
         
         return (
             <Layout>
                 <h1>To Do List</h1>
                 
-                {/* Display empty notice if list is empty */}
-                {todoList.length === 0? emptyNotice : ""}
-                <ul>
-                    {todoList.map((item, index) => {
-                        return (
-                                <li key={index}>{item.todo}
-                                    <form action={`/todo/${item._id}?_method=DELETE`} method="post">
-                                        <input type="submit" value="DELETE"/>
-                                    </form>
-                                </li>
-                        );
-                    })}
-                </ul>
-                
-                <hr></hr>
-                
-                <form action="/todo" method="POST">
-                    <input type="text" name="todo" required/>
-                    <input type="submit" value="Add To Do"/>
-                </form>
+                {/* 
+                Check for a database connection.
+                If it's active, check for an empty list.
+                Display empty notice if list is empty 
+                */}
+
+                {todoList == 'noDatabase'? noDatabaseNotice 
+                : todoList.length === 0? emptyNotice 
+                :
+
+                // Show the todos and ADD buttons if everything is working
+                <div>
+                    <ul>
+                        {todoList.map((item, index) => {
+                            return (
+                                    <li key={index}>{item.todo}
+                                        <form action={`/todo/${item._id}?_method=DELETE`} method="post">
+                                            <input type="submit" value="DELETE"/>
+                                        </form>
+                                    </li>
+                            );
+                        })}
+                    </ul>
+
+                    <hr></hr>
+                    
+                    {/* Add todos here */}
+                    <form action="/todo" method="POST">
+                        <input type="text" name="todo" required/>
+                        <input type="submit" value="Add To Do"/>
+                    </form>
+                </div>
+                }
             </Layout>
         );
     };
